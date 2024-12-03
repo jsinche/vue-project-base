65dc10215939075bfd588fed4981f1f277a15c7e
Jean <jean.sinche@alarcongroup.us>
2024-12-03 17:29:10 -0500

    Adds PrimeVue UI library
    Installs PrimeVue and PrimeIcons packages.
    
    Updates the main app file to include the PrimeVue configuration and
    ripple directive.
    
    Removes existing styling and default components to prepare for PrimeVue
    components integration.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Ripple from 'primevue/ripple';
 
 import App from './App.vue'
 import router from './router'
-
+// import "primevue/resources/themes/lara-light-green/theme.css"
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
 const app = createApp(App)
-
+app.use(PrimeVue, { ripple: true });
 app.use(createPinia())
 app.use(router)
 
+app.directive('ripple', Ripple);
 app.mount('#app')
