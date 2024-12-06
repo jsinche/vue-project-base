ef0d5342fbedc0604283451427b88da17e859bf5
JeanS <jean.sinche@alarcongroup.us>
2024-12-06 16:06:59 -0500

    feat: Implements basic user creation modal
    Adds a modal for creating users, accessible from the user list page.
    
    This allows users to be created within the application.

---
+++ src/modules/users/components/UpsertUserModal.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import MyCustomModal from '@/components/Modal/MyCustomModal.vue';
+import Button from 'primevue/button';
+interface Emits {
+  (event: "close"): void;
+}
+const emits = defineEmits<Emits>();
+</script>
+<template>
+  <MyCustomModal
+    title="Pruebitas"
+    icon="pi pi-check-circle"
+    width="40rem"
+    @close="emits('close')"
+  >
+    <template #content>
+      <div>Pruebitas</div>
+    </template>
+    <template #footer>
+      <Button label="Close" severity="danger" @click="emits('close')" />
+      <Button label="Save" severity="success" @click="emits('close')" />
+    </template>
+  </MyCustomModal>
+</template>
+<style lang="scss" scoped>
+</style>
--- src/modules/users/pages/UserList.vue
+++ src/modules/users/pages/UserList.vue
@@ -1,11 +1,20 @@
 <script lang="ts" setup>
-
+import Button from 'primevue/button';
+import UpsertUserModal from '../components/UpsertUserModal.vue';
+import { ref } from 'vue';
+const showModal = ref(false);
 </script>
 <template>
-  <div>UserList</div>
+ <div>Uwu</div>
+  <Button
+    label="Crear Usuario"
+    icon="pi pi-user-plus"
+    severity="success"
+    @click="showModal = true" />
+  <UpsertUserModal
+    v-if="showModal"
+    @close="showModal = false"
+  />
 </template>
-
-
 <style lang="scss" scoped>
-
 </style>
