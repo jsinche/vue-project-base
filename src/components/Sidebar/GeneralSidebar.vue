66023b83007d221fa08e22938081e8a0278571a3
JeanS <jean.sinche@alarcongroup.us>
2024-12-05 19:14:09 -0500

    Improves sidebar navigation and styling
    Updates sidebar styling for improved UI and expands/collapses sub-items.
    
    Adds active state highlighting to currently active parent menu item.
    
    Changes default dashboard link to /dashboard/users/list.

--- src/components/Sidebar/GeneralSidebar.vue
+++ src/components/Sidebar/GeneralSidebar.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { ref, computed } from 'vue'
 import { routerLinks } from '@/router/dashboard-routes-list';
+import { useRoute } from 'vue-router';
+const route = useRoute();
 const isExpanded = ref(false);
 const isPinned = ref(false);
 const showSidebarDetails = computed(() => isExpanded.value || isPinned.value);
@@ -41,7 +43,7 @@ const menuItems = ref<MenuItem[]>([
     title: 'Dashboard',
     isOpen: false,
     subitems: [
-      { id: 11, title: 'Overview', link: '/dashboard', icon: 'pi pi-home' },
+      { id: 11, title: 'Overview', link: '/dashboard/users/list', icon: 'pi pi-home' },
       { id: 12, title: 'Analytics', link: '/dashboard/analytics', icon: 'pi pi-chart-bar' },
     ]
   },
@@ -83,6 +85,11 @@ const endTransition = (el: Element) => {
   void element.offsetHeight
   element.style.height = '0'
 }
+
+const isParentActive = (item: MenuItem) => {
+  console.log(route.path);
+  return item.subitems?.some((subitem) => subitem.link === route.path);
+};
 </script>
 <template>
   <aside class="sidebar" :class="{ 'sidebar--expanded': showSidebarDetails }" @mouseenter="handleMouseEnter"
@@ -93,37 +100,32 @@ const endTransition = (el: Element) => {
       </h2>
       <hr class="sidebar__hr" />
       <nav class="sidebar__nav">
-        <ul class="sidebar-list">
-          <li v-for="item in menuItems" :key="item.id" class="sidebar-item">
-            <div
-              class="sidebar__nav-link"
-              :class="{ 'sidebar-item--is-open': item.isOpen }"
-              @click="toggleSubitems(item)"
-            >
-              <i :class="item.icon" class="sidebar__nav-link-icon"></i>
+        <ul class="sidebar__list">
+          <li v-for="item in menuItems" :key="item.id">
+            <div class="sidebar__list-item" :class="{
+              'sidebar__list-item--is-open': item.isOpen,
+              'sidebar__list-item--is-active': isParentActive(item)
+            }" @click="toggleSubitems(item)">
+              <i :class="item.icon" class="sidebar__list-item-icon"></i>
               <transition name="fade">
-                <span
-                  class="sidebar__nav-link-text"
-                  v-if="showSidebarDetails"
-                  :aria-expanded="item.isOpen"
-                >{{ item.title }}</span>
+                <span class="sidebar__list-item-text" v-if="showSidebarDetails" :aria-expanded="item.isOpen">
+                  {{ item.title }}
+                  <i class="pi pi-angle-down angle-icon" :class="{ 'angle-icon--is-open': item.isOpen }"></i>
+                </span>
               </transition>
             </div>
-              <transition
-                @enter="startTransition"
-                @leave="endTransition"
-                >
-                <ul v-if="item.isOpen" class="sidebar-sublist">
-                  <li v-for="subitem in item.subitems" :key="subitem.id" class="sidebar-subitem">
-                    <RouterLink :to="subitem.link" class="sidebar__nav-link">
-                      <i :class="subitem.icon" class="sidebar__nav-link-icon"></i>
-                      <transition name="fade">
-                        <span class="sidebar__nav-link-text" v-if="showSidebarDetails">{{ subitem.title }}</span>
-                      </transition>
-                    </RouterLink>
-                  </li>
-                </ul>
-              </transition>
+            <transition @enter="startTransition" @leave="endTransition">
+              <ul v-if="item.isOpen" class="sidebar__sublist">
+                <li v-for="subitem in item.subitems" :key="subitem.id" class="sidebar__sublist-item">
+                  <RouterLink :to="subitem.link" class="sidebar__sublist-item-link">
+                    <i :class="subitem.icon" class="sidebar__sublist-item-icon"></i>
+                    <transition name="fade">
+                      <span class="sidebar__sublist-item-text" v-if="showSidebarDetails">{{ subitem.title }}</span>
+                    </transition>
+                  </RouterLink>
+                </li>
+              </ul>
+            </transition>
           </li>
         </ul>
       </nav>
@@ -140,57 +142,6 @@ const endTransition = (el: Element) => {
   </aside>
 </template>
 <style lang="scss" scoped>
-.sidebar-list {
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-
-}
-.sidebar-item {
-  margin-bottom: 4px;
-}
-
-
-.sidebar-item--is-open {
-  background-color: var(--secondary-500);
-}
-
-.sidebar-icon {
-  width: 20px;
-  height: 20px;
-  transition: transform 0.3s ease;
-}
-
-.sidebar-button.is-open .sidebar-icon {
-  transform: rotate(180deg);
-}
-
-.sidebar-sublist {
-  list-style-type: none;
-  padding-left: 15px;
-  margin-top: 4px;
-  overflow: hidden;
-  transition: height 0.3s ease;
-}
-
-.sidebar-subitem {
-  margin-bottom: 4px;
-}
-
-.sidebar-link {
-  display: block;
-  padding: 8px 15px;
-  color: #4b5563;
-  text-decoration: none;
-  font-size: 14px;
-  border-radius: 4px;
-  transition: background-color 0.3s ease;
-}
-
-.sidebar-link:hover {
-  background-color: #e5e7eb;
-}
-
 nav a.router-link-active {
   background-color: var(--secondary-500);
 }
@@ -235,8 +186,85 @@ nav a.router-link-active {
     gap: 1rem;
   }
 
-  &__pin,
-  &__nav-link {
+  &__list {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+
+    &-item {
+      margin: 0.5rem;
+      cursor: pointer;
+      padding: 0.8rem;
+      color: white;
+      text-decoration: none;
+      border-radius: 0.25rem;
+      transition: background-color 0.3s;
+      display: flex;
+      align-items: center;
+      &:hover {
+        background-color: var(--white-20);
+      }
+      &--is-open {
+        background-color: var(--white-20);
+      }
+
+      &--is-active {
+        background-color: var(--secondary-500);
+        &:hover {
+          background-color: var(--secondary-500);
+        }
+      }
+
+      &-text {
+        margin-left: 1rem;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+      }
+
+      &-icon {
+        font-size: 2rem;
+      }
+
+    }
+  }
+
+  &__sublist {
+    list-style-type: none;
+    padding-left: 15px;
+    margin-top: 4px;
+    overflow: hidden;
+    transition: height 0.3s ease;
+
+    &-item {
+      margin: 0.5rem;
+      border-radius: 0.25rem;
+
+      &-link {
+        padding: 1rem;
+        color: white;
+        text-decoration: none;
+        border-radius: 0.25rem;
+        display: flex;
+        align-items: center;
+      }
+
+      &-text {
+        margin-left: 1rem;
+      }
+
+      &-icon {
+        font-size: 1.5rem;
+      }
+
+      &:hover {
+        border-radius: 0.25rem;
+        background-color: var(--white-20);
+      }
+    }
+  }
+
+  &__pin {
     padding: 0.8rem;
     color: white;
     text-decoration: none;
@@ -244,6 +272,7 @@ nav a.router-link-active {
     transition: background-color 0.3s;
     display: flex;
     align-items: center;
+    cursor: pointer;
 
     &-text {
       margin-left: 1rem;
@@ -254,7 +283,7 @@ nav a.router-link-active {
     }
 
     &:hover {
-      background-color: var(--secondary-500)
+      background-color: var(--white-20)
     }
   }
 
@@ -267,6 +296,14 @@ nav a.router-link-active {
 
     &-logo {
       width: 60px;
+    }
+  }
+
+  .angle-icon {
+    transition: transform 0.3s ease;
+
+    &--is-open {
+      transform: rotate(180deg);
     }
   }
 }
